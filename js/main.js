1e42be1052539f7318d5b2e256ff8627cb7b788b
AlexABA91 <80538104+AlexABA91@users.noreply.github.com>
2021-08-23 11:13:03 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
-$(function() {
-	
-	
-	
-	
-});
+let images = document.images;
+let imagesTotalCaunt = images.length;
+let imagesLoaderCaunt = 0;
+let persDisplay = document.getElementById('prosentage')
+let preloader = document.getElementById('preloder')
+for (i = 0; i < imagesTotalCaunt; i++) {
+   imageClone = new Image();
+   imageClone.onload = image_loaded;
+   imageClone.onerror = image_loaded;
+   imageClone.src = images[i].src
+}
+console.log(imagesTotalCaunt);
+console.log(imagesLoaderCaunt);
+console.log(images)
+function image_loaded() {
+   imagesLoaderCaunt++;
+   persDisplay.innerHTML = (((100 / imagesTotalCaunt) * imagesLoaderCaunt) << 0) + '%';
+   if (imagesLoaderCaunt >= imagesTotalCaunt) {
+      document.body.onload = function () {
+         setTimeout(() => {
+            if (!preloader.classList.contains('done')) {
+               preloader.classList.add('done')
+            }
+         }, 2000);
+      }
+   }
+}
+
